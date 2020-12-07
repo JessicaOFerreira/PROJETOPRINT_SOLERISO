ce1d760076aa88217f707adcfd268606217b1827
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2020-12-07 15:36:04 -0300

    Creating connection class and database configuration

---
+++ SolERiso/src/sql/DBSettings.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sql;
+
+/**
+ *
+ * @author johnn
+ */
+public class DBSettings {
+    public static final String DB_NAME = "MYSQL";
+    public static String DB_URL = "";
+    public static String DB_USER = "root";
+    public static String DB_PASSWORD = "";
+}
