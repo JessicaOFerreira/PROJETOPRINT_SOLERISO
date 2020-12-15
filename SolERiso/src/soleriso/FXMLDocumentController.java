f764f02c07b93f5c050d81516845ae7e2708c29f
Johnny Ferreira <46564172+johnnyferreiradev@users.noreply.github.com>
2020-12-14 21:32:39 -0300

    Feat/db connection (#1)
    * Creating connection class and database configuration
    
    * Bugfixing database connection erros
    
    * Creating Admin DAO and login method

--- SolERiso/src/soleriso/FXMLDocumentController.java
+++ SolERiso/src/soleriso/FXMLDocumentController.java
@@ -5,12 +5,18 @@
  */
 package soleriso;
 
+import dao.DaoAdmin;
+import dao.IDaoAdmin;
+import entities.Admin;
+import exceptions.DaoException;
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import sql.SQLConnection;
 
 /**
  *
@@ -18,18 +24,29 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    private Connection connection;
+    private final IDaoAdmin daoAdmin = new DaoAdmin();
+    private Admin admin;
+    
     @FXML
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World, Sol&Riso!");
+    private void handleButtonAction(ActionEvent event) throws DaoException {
+        // System.out.println("You clicked me!");
+        // label.setText("Hello World, Sol&Riso!");
+        
+        admin = daoAdmin.login("admin@admin.com", "mudar123");
+        
+        System.out.println(admin.getLogin());
+        System.out.println(admin.getIsDentist());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        System.out.println("Initialize");
+        this.connection = SQLConnection.getConnectionInstance();
     }    
     
 }
